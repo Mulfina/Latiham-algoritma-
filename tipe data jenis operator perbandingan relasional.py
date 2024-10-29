8dc2cb407b9e291ed313fd58d459827d23e13b73
Mulfina <mulfina77@gmail.com>
2024-10-29 23:21:47 +0800

    Add files via upload

---
+++ tipe data jenis operator perbandingan relasional.py
@@ -0,0 +1,23 @@
+a = 5
+b = 5
+print(a == b)  # Output: True
+
+a = 5
+b = 3
+print(a != b)  # Output: True
+
+a = 7
+b = 5
+print(a > b)  # Output: True
+
+a = 3
+b = 5
+print(a < b)  # Output: True
+
+a = 5
+b = 5
+print(a >= b)  # Output: True
+
+a = 4
+b = 5
+print(a <= b)  # Output: True
